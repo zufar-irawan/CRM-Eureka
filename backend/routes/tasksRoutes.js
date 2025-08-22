30aa42e803c4661b2126c233effe2818826550f3
FzeeyNa <zeerayzan@gmail.com>
2025-08-22 08:57:17 +0700

    anjay mabar

--- backend/routes/tasksRoutes.js
+++ backend/routes/tasksRoutes.js
@@ -25,18 +25,17 @@ import {
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = Router();
-router.get("/", getTasks);                           // GET /api/tasks - List tasks (dengan filter lead_id, assigned_to, dll)
-router.post("/", authMiddleware, createTask);                        // POST /api/tasks - Tambah task
-router.get("/:id", getTaskById);                     // GET /api/tasks/:id - Detail task
-router.put("/:id", updateTask);                      // PUT /api/tasks/:id - Update task
-router.delete("/:id", deleteTask);                   // DELETE /api/tasks/:id - Delete task
-router.put('/:id/updateStatus', updateTaskStatus);   // PUT /api/tasks/:id/updateStatus - Update status task
-router.get("/:id/comments", getTaskComments);        // GET /api/tasks/:id/comments - Ambil semua komentar pada task tertentu
-router.post("/:id/comments", addTaskComment);        // POST /api/tasks/:id/comments - Tambahkan komentar ke task
-router.put("/task-comments/:commentId", updateTaskComment);    // PUT /api/tasks/task-comments/:commentId - Edit komentar tertentu
-router.delete("/task-comments/:commentId", deleteTaskComment); // DELETE /api/tasks/task-comments/:commentId - Hapus komentar tertentu
-router.get("/:id/results", getTaskResults);          // GET /api/tasks/:id/results - Ambil semua hasil pada task tertentu
-router.post("/:id/results", addTaskResult);
+router.get("/", getTasks);                          
+router.post("/", authMiddleware, createTask);     
+router.get("/:id", getTaskById);                   
+router.put("/:id", updateTask);                     
+router.delete("/:id", deleteTask);                   
+router.put('/:id/updateStatus', updateTaskStatus);  
+router.get("/:id/comments", getTaskComments);       
+router.post("/:id/comments", addTaskComment);      
+router.put("/task-comments/:commentId", updateTaskComment);   
+router.delete("/task-comments/:commentId", deleteTaskComment);
+router.get("/:id/results", getTaskResults);         
 router.put("/task-results/:resultId", updateTaskResult);
 router.delete("/task-results/:resultId", deleteTaskResult);
 router.post(
